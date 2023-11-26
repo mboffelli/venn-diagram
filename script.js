e8807daa9ea56ca0eff07c33a5da8d0ae1400315
Mayuko Boffelli <mboffelli@ucla.edu>
2023-11-26 10:24:56 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.querySelectorAll('.circle').forEach(item => {
   item.addEventListener('mouseover', () => {
-    // Increase the size of the circle
-    item.style.transform = 'scale(1.2)';
+    item.style.transform = 'scale(1.1)';
+    item.style.opacity = '1';
     item.style.zIndex = '10';
   });
 
   item.addEventListener('mouseout', () => {
-    // Reset the size of the circle
     item.style.transform = 'scale(1)';
+    item.style.opacity = '0.7';
     item.style.zIndex = '1';
   });
 });
